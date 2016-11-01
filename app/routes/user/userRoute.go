c3c3e522e0eabbeee4ceba3a0cb4295272246cd0
Pranesh Kumar.R <praneshkmr@gmail.com>
2016-11-01 14:19:08 +0530

    Endpoint to GET user by ID

--- app/routes/user/userRoute.go
+++ app/routes/user/userRoute.go
@@ -3,6 +3,7 @@ package user
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"./../../../app/model"
 	"github.com/gorilla/mux"
@@ -15,21 +16,49 @@ func addUser(res http.ResponseWriter, req *http.Request) {
 	var user model.User
 	err := decoder.Decode(&user)
 	if err != nil {
-		panic(err)
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("Failed to Decode Request Body"))
+		return
 	}
 	addedUser, err := UserService.AddUser(&user)
 	if err != nil {
-		panic(err)
+		res.WriteHeader(http.StatusInternalServerError)
+		res.Write([]byte("Failed to Add User"))
+		return
 	}
 	res.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	res.WriteHeader(http.StatusCreated)
 	if err := json.NewEncoder(res).Encode(addedUser); err != nil {
-		panic(err)
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("Failed to Encode Response Body"))
+	}
+}
+
+func getUserByID(res http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	userIDStr := vars["id"]
+	userID, err := strconv.Atoi(userIDStr)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("Usage -> /user/:ID where ID is an Integer"))
+		return
+	}
+	user, err := UserService.GetUserByID(userID)
+	if err != nil {
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
+	res.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	res.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(res).Encode(user); err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("Failed to Encode Response Body"))
 	}
 }
 
 // AssignRoutes is used to Setup the REST routes with the appropriate Handlers
 func AssignRoutes(router *mux.Router) {
 	router.HandleFunc("/user", addUser).Methods("POST")
+	router.HandleFunc("/user/{id}", getUserByID).Methods("GET")
 	http.ListenAndServe(":3000", router)
 }
