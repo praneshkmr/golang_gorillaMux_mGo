39ea315a6677f661301177fd68466605402db5e7
Pranesh Kumar.R <praneshkmr@gmail.com>
2016-11-01 14:42:40 +0530

    Setting the JSON response keys to lower cases

--- app/model/user.go
+++ app/model/user.go
@@ -4,9 +4,9 @@ import "gopkg.in/mgo.v2/bson"
 
 //User is the Data Model to store user related data
 type User struct {
-	ID       int           `bson:"id"`
+	ID       int           `bson:"id" json:"id"`
 	_ID      bson.ObjectId `bson:"_id,omitempty"`
-	Name     string        `bson:"name"`
-	Email    string        `bson:"email"`
-	Password string        `bson:"password"`
+	Name     string        `bson:"name" json:"name"`
+	Email    string        `bson:"email" json:"email"`
+	Password string        `bson:"password" json:"password"`
 }
